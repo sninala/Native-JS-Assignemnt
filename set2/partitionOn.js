c2f8fa15fa3681cdd70b55064558243ce7d35289
Siva Ninala <Siva_Ninala@epam.com>
2017-04-10 22:53:21 +0530

    updating the code as per code review comments

--- set2/partitionOn.js
+++ set2/partitionOn.js
@@ -3,8 +3,12 @@
 function partitionOn(pred, items) {
     var false_idx = 0
     for (var i = 0; i < items.length; i++) {
-        if (!pred(items[i]))
-            items.splice(false_idx++, 0, items.splice(i, 1)[0])
+        var currentItem = items[i];
+        if (!pred(currentItem)) {
+            items.splice(i, 1); //remove current item for index i
+            items.splice(false_idx, 0, currentItem); //add current item at false index
+            false_idx += 1;
+        }
     }
     return false_idx
 }
