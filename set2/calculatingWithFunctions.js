61b177c995c94f84dea5577de8871ec361bcf3f9
Siva Ninala <Siva_Ninala@epam.com>
2017-04-10 11:46:00 +0530

    updated the code as per review comments..

--- set2/calculatingWithFunctions.js
+++ set2/calculatingWithFunctions.js
@@ -1,53 +1,40 @@
-//-	http://www.codewars.com/kata/calculating-with-functions
-
-function tokenize(num, func) {
-  if (!func) {
-		return num;
-	} else {
-		return func(num);
-	}
+function calculate(num) {
+    return function (func) {
+        return func ? func(num) : num;
+    }
 }
 
-function zero(func) {
-	return tokenize(0, func);
-}
-function one(func) {
-	return tokenize(1, func);
-}
-function two(func) {
-	return tokenize(2, func);
-}
-function three(func) {
-	return tokenize(3, func);
-}
-function four(func) {
-	return tokenize(4, func);
-}
-function five(func) {
-	return tokenize(5, func);
-}
-function six(func) {
-	return tokenize(6, func);
-}
-function seven(func) {
-	return tokenize(7, func);
-}
-function eight(func) {
-	return tokenize(8, func);
-}
-function nine(func) {
-	return tokenize(9, func);
-}
+var zero = calculate(0);
+var one = calculate(1);
+var two = calculate(2);
+var three = calculate(3);
+var four = calculate(4);
+var five = calculate(5);
+var six = calculate(6);
+var seven = calculate(7);
+var eight = calculate(8);
+var nine = calculate(9);
 
 function plus(v) {
-  return function(u) { return u + v; };
+    return function (u) {
+        return u + v;
+    };
 }
-function minus(v) { 
-    return function(u) { return u - v; };
+
+function minus(v) {
+    return function (u) {
+        return u - v;
+    };
 }
+
 function times(v) {
-  return function(u) { return v * u; };
+    return function (u) {
+        return v * u;
+    };
 }
+
 function dividedBy(v) {
-  return function(u) { return u / v; };
+    return function (u) {
+        return u / v;
+    };
 }
