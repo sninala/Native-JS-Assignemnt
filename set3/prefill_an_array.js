4eaa5be4d8441dd6dd3fd2a7656e6f7f6acad191
Siva Ninala <Siva_Ninala@epam.com>
2017-04-10 11:47:07 +0530

    simplified the logic to understand the program easily ..

--- set3/prefill_an_array.js
+++ set3/prefill_an_array.js
@@ -1,12 +1,14 @@
 // -	http://www.codewars.com/kata/prefill-an-array
 
 function prefill(n, v) {
-    if (/\D+/g.test(n) || n < 0) {
+    var n1 = Number(n);
+    if (n >= 0) {
+        var elements = [];
+        for (var i = 0; i < n; i++) {
+            elements.push(v);
+        }
+        return elements;
+    } else {
         throw new TypeError(n + ' is invalid')
     }
-    var elements = [];
-    for (var i = 0; i < n; i++) {
-        elements.push(v);
-    }
-    return elements;
 }
