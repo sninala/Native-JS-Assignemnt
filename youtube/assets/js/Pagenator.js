5915e3b910b715d8418861b4eb6f2ca5cd84699b
Siva Ninala <Siva_Ninala@epam.com>
2017-05-17 21:21:52 +0530

    adding the Pagenator.js to github ..

---
+++ youtube/assets/js/Pagenator.js
@@ -0,0 +1,33 @@
+var Pagenator = function () {};
+
+Pagenator.prototype.getTotalNumberOfPagesFor = function (items) {
+    var numberOfVideosToDisplay = this.getNumberOfVideosForCurrentPage()
+    var totalVideos = items.length;
+    var numberOfpages = Math.floor(totalVideos / numberOfVideosToDisplay);
+    var additionalPagesToAdd = totalVideos % numberOfVideosToDisplay === 0 ? 0 : 1;
+    numberOfpages += additionalPagesToAdd;
+    return numberOfpages;
+}
+
+Pagenator.prototype.getNumberOfVideosForCurrentPage = function () {
+    var availablePageWidth = window.innerWidth;
+    var minimumNumberOfVideosPerPage = AppConstants.MIN_VIDEOS_PER_PAGE;
+    var videoWidth = AppConstants.VIDEO_WIDTH;
+    while ((minimumNumberOfVideosPerPage * videoWidth) < availablePageWidth) {
+        minimumNumberOfVideosPerPage += 1;
+    }
+    return (minimumNumberOfVideosPerPage > 1) ? minimumNumberOfVideosPerPage - 1 : 1;
+
+}
+Pagenator.prototype.getCurrentPage = function () {
+    return this.currentPage || 1;
+}
+
+Pagenator.prototype.setCurrentPage = function (pageNumber) {
+    this.currentPage = pageNumber;
+}
+
+Pagenator.prototype.getStartIndexForPage = function (numberOfCards) {
+    var currentPage = this.getCurrentPage();
+    return (currentPage * numberOfCards) - numberOfCards
+}
